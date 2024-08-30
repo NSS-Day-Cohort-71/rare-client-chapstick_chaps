b0ef08e7e05766941e16edb98f122518cdd21cfa
Caleb Ford <caleb.ford.sto@gmail.com>
2024-08-30 16:01:06 -0500

    working commit

--- src/components/posts/myPosts/MyPosts.js
+++ src/components/posts/myPosts/MyPosts.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { getPostsByUserId } from "../../../managers/PostManager"
-
+import "./MyPosts.css"
 export const MyPosts = ({currentUser}) => {
     const [userPosts, setUserPosts] = useState([])
 
@@ -13,9 +13,42 @@ export const MyPosts = ({currentUser}) => {
         })
     }
     
+    const formatDate = (datetime) => {
+        return datetime ? datetime.split(" ")[0] : ""; 
+    };
+
     useEffect(()=>{
         const id = currentUser.id
         getAndSetUserPosts(id)
     },[currentUser])
-    return <button className="button" onClick={()=> {navigate("/newPostForm")}}>New Post</button>
+
+
+    return <div className="my-post-view">
+        <div className="button-box">
+            <button className="button" onClick={()=> {navigate("/newPostForm")}}>New Post</button>
+        </div>
+        
+        <div className="posts">
+            {userPosts.map((post) => {
+                
+            return <div key={post.id} className="one-post">
+                <div className="post-header">
+                    <h3>{post.title}</h3>
+                    <h4>{formatDate(post.publication_date)}</h4>
+                </div>
+                <div className="post-img">
+                    <img src={post.image_url} alt={post.title}/>
+                </div>
+                <div className="post-footer">
+                    <span>Author: {post.user.username}</span>
+                    <div className="footer-buttons">
+                    <span>reaction placeholder</span>
+                    <button className="button">Edit</button>
+                    <button className="button">Delete</button>
+                    </div>
+                </div>
+            </div>})}
+        </div>
+        
+        </div>
 }
