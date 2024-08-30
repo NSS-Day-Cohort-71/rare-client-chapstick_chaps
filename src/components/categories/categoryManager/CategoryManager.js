1342d0c645809808a25630945552ac5b1fba552b
Nicholas Winkler <nhwinkler@gmail.com>
2024-08-30 14:04:07 -0500

    Updated the Category Manager to display all Categories

--- src/components/categories/categoryManager/CategoryManager.js
+++ src/components/categories/categoryManager/CategoryManager.js
@@ -1,29 +1,19 @@
-// Given an admin is in the app
-// When they select the Category Management link in the menu
-// Then they should be directed to a page that lists all the Category names ordered alphabetically
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getCategories } from "../../../managers/CategoryManager";
-import styles from "./CategoryManager.css"; // Import CSS module
+import styles from "./CategoryManager.css";
 
 export const CategoryManager = () => {
   const [categories, setCategories] = useState([]);
 
   const fetchAndSetCategories = () => {
     getCategories().then((data) => {
-      // Sort categories alphabetically by label
       const sortedCategories = data.sort((a, b) =>
         a.label.localeCompare(b.label)
       );
       setCategories(sortedCategories);
     });
   };
-
-  // const handleDelete = (categoryId) => {
-  //   deleteCategory(categoryId).then(() => {
-  //     fetchAndSetCategories(); // Refresh the list after deletion
-  //   });
-  // };
 
   useEffect(() => {
     fetchAndSetCategories();
@@ -32,32 +22,30 @@ export const CategoryManager = () => {
   const navigate = useNavigate();
 
   return (
-    <div className={styles.CategoryManager}>
-      <h1>Category Manager</h1>
-      <ul>
+    <div className={styles.categoryView}>
+      <div className={styles.header}>
+        <h1>Category Manager</h1>
+        <button
+          className={styles.newCategoryButton}
+          onClick={() => navigate("/CreateCategory")}
+        >
+          Add Category
+        </button>
+      </div>
+      <ul className={styles.categoryListBox}>
         {categories.map((category) => (
-          <li key={category.id}>
+          <li className={styles.oneCategory} key={category.id}>
+            <button
+              className={styles.button}
+              onClick={() => navigate(`/editCategory/${category.id}`)}
+            >
+              Edit
+            </button>
+            <button className={styles.button}>Delete</button>
             {category.label}
-            <div>
-              <button
-                onClick={() => {
-                  navigate(`/editCategory/${category.id}`);
-                }}
-              >
-                Edit
-              </button>
-              {/* <button onClick={() => handleDelete(category.id)}>Delete</button> */}
-            </div>
           </li>
         ))}
       </ul>
-      <button
-        onClick={() => {
-          navigate("/CreateCategory");
-        }}
-      >
-        Add Category
-      </button>
     </div>
   );
 };
